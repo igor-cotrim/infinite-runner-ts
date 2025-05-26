1677fb377425ca12a55ec7b051eaed1610dd655e
Igor Cotrim Santos <igorcotrim.dev@gmail.com>
2025-05-26 19:42:01 -0300

    feat: :sparkles: create a Player Entity

---
+++ src/entities/Player.ts
@@ -0,0 +1,28 @@
+class Player {
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  color: string;
+
+  constructor(
+    x: number,
+    y: number,
+    width: number,
+    height: number,
+    color: string
+  ) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.color = color;
+  }
+
+  draw(ctx: CanvasRenderingContext2D): void {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+export default Player;
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,24 @@
+import Player from "./entities/Player";
+
 import "./style.css";
 
 class Game {
   canvas = document.getElementById("game-canvas") as HTMLCanvasElement;
   ctx = this.canvas.getContext("2d")!;
+  player: Player;
 
   constructor() {
     this.canvas.width = window.innerWidth;
     this.canvas.height = window.innerHeight;
+
+    this.player = new Player(50, this.canvas.height - 50, 50, 50, "#f231a5");
   }
 
   render() {
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-
     this.ctx.fillStyle = "#0a0c21";
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+
+    this.player.draw(this.ctx);
   }
 }
 
